b8f770ac0166a67fc8eb3eb74e3f82d94045ddbe
Juan Andrés <juabo008@gmail.com>
2023-02-09 18:29:34 -0500

    Avance 2 de django

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import  get_object_or_404, render
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello world</h1>")
+def index(request):
+    return render(request, 'index.html')
+
+def hello(request, username):
+    return HttpResponse("<h1>Hello %s</h1>" %username)
 
 
 def about(request):
     return HttpResponse("About");
+
+
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+                        
+def tasks(request,title):
+    task= Task.objects.get(title=title)
+    return HttpResponse("Tasks: %s" % task.id)
+
